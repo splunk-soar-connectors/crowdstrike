31229d35b861471de73152da0e1a8178ffadaa46
achandya <achandya@splunk.com>
2020-09-15 18:33:30 +0530

    Crowdstrike Streaming API: Bug Fixation of PAPP-10409.

--- crowdstrike_consts.py
+++ crowdstrike_consts.py
@@ -19,6 +19,8 @@ CROWDSTRIKE_ERR_CONNECTING = "Error connecting to server"
 CROWDSTRIKE_ERR_FROM_SERVER = "Error from Server, Status Code: {status}, Message: {message}"
 CROWDSTRIKE_UNABLE_TO_PARSE_DATA = "Unable to parse data from server"
 CROWDSTRIKE_ERR_EVENTS_FETCH = "Error occurred while fetching the DetectionSummaryEvents from the CrowdStrike server datafeed URL stream"
+CROWDSTRIKE_LIMIT_VALIDATION_ALLOW_ZERO_MSG = "Please provide zero or a valid positive integer value in the {parameter} parameter"
+CROWDSTRIKE_LIMIT_VALIDATION_MSG = "Please provide a valid non-zero positive integer value in the {parameter} parameter"
 
 # Progress messages format string
 CROWDSTRIKE_USING_BASE_URL = "Using base url: {base_url}"
